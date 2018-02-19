bcd67a857202e93777ebf01323a7810ccaf04b63
Nik Lakhani <kankotiya.ashvin@gmail.com>
2018-02-19 14:49:09 +0530

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+(function($) {
+    $('#awa-skills-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        centerMode: true
+    });
+})(jQuery);
