28874d08439d5859b6080d04f587e9be3a5f3572
Nik Lakhani <kankotiya.ashvin@gmail.com>
2018-02-19 15:54:49 +0530

    update!

--- js/app.js
+++ js/app.js
@@ -4,6 +4,27 @@
         slidesToShow: 3,
         slidesToScroll: 1,
         arrows: false,
-        centerMode: true
+        centerMode: true,
+        asNavFor: '#awa-skills-text-slider'
+    });
+
+    $('#awa-skills-text-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        cssEase: 'linear',
+        asNavFor: '#awa-skills-slider'
+    });
+
+    $('#awa-skill-prev').on('click', function(e) {
+        e.preventDefault();
+        $('#awa-skills-slider').slick('slickPrev');
+    });
+
+    $('#awa-skill-next').on('click', function(e) {
+        e.preventDefault();
+        $('#awa-skills-slider').slick('slickNext');
     });
 })(jQuery);
